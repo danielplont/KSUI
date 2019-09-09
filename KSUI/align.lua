30cb2ff22142b142df8b8a498d0651f6663aaf75
Daniel Plont <dplont@omnicogroup.com>
2019-09-09 15:11:36 -0400

    align cleanup

--- KSUI/align.lua
+++ KSUI/align.lua
@@ -6,47 +6,37 @@ local f
 
 SlashCmdList["EA"] = function()
 
-   if f then
-      
-	   f:Hide()
-      f = nil		
-      
-   else
-
+	if f then
+    	f:Hide()
+    	f = nil		
+	else
+		
 		f = CreateFrame('Frame', nil, UIParent) 
-      f:SetAllPoints(UIParent)
+    	f:SetAllPoints(UIParent)
       
 		local w = GetScreenWidth() / 64
-      local h = GetScreenHeight() / 36
+    	local h = GetScreenHeight() / 36
 
-      for i = 0, 64 do
-         
-         local t = f:CreateTexture(nil, 'BACKGROUND')
-         
+      	for i = 0, 64 do
+        	local t = f:CreateTexture(nil, 'BACKGROUND')
 			if i == 32 then
 				t:SetColorTexture(1, 1, 0, 0.5)
 			else
 				t:SetColorTexture(1, 1, 1, 0.15)
-         end
-         
-			t:SetPoint('TOPLEFT', f, 'TOPLEFT', i * w - 1, 0)
-         t:SetPoint('BOTTOMRIGHT', f, 'BOTTOMLEFT', i * w + 1, 0)
-         
-      end
+        	end
+        	t:SetPoint('TOPLEFT', f, 'TOPLEFT', i * w - 1, 0)
+			t:SetPoint('BOTTOMRIGHT', f, 'BOTTOMLEFT', i * w + 1, 0)
+        end
       
-      for i = 0, 36 do
-         
-         local t = f:CreateTexture(nil, 'BACKGROUND')
-         
-			if i == 18 then
-				t:SetColorTexture(1, 1, 0, 0.5)
-			else
-				t:SetColorTexture(1, 1, 1, 0.15)
-         end
-         
+    	for i = 0, 36 do  
+        	local t = f:CreateTexture(nil, 'BACKGROUND')
+        		if i == 18 then
+					t:SetColorTexture(1, 1, 0, 0.5)
+				else
+					t:SetColorTexture(1, 1, 1, 0.15)
+        	 	end
 			t:SetPoint('TOPLEFT', f, 'TOPLEFT', 0, -i * h + 1)
-         t:SetPoint('BOTTOMRIGHT', f, 'TOPRIGHT', 0, -i * h - 1)
-         
+    		t:SetPoint('BOTTOMRIGHT', f, 'TOPRIGHT', 0, -i * h - 1)
 		end	
 	end
 end
